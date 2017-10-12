1ba6eed6d8d824ff9bbc8f52de714d81a9a3dd68
UMB200 <tomodachi200@gmail.com>
2017-10-12 14:36:10 -0400

    upload solution

--- orbitalPeriod.js
+++ orbitalPeriod.js
@@ -2,7 +2,12 @@
 function orbitalPeriod(arr) {
   var GM = 398600.4418;
   var earthRadius = 6367.4447;
-  return arr;
+ 
+  //using formula t=2*pi(Squire root of (a*a*a)/GM)
+  return arr.reduce((all, item) => {
+    var formula = Math.round(2*Math.PI*(Math.sqrt(Math.pow(earthRadius + item.avgAlt, 3)/GM)));
+    all.push({name: item.name, orbitalPeriod: formula});
+    return all;
+  }, []);
 }
-
 orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
